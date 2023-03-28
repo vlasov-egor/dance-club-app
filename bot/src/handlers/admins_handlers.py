ee7688fdcf55fb68de45c20f9353975a976f581f
vlasov-egor <vlasov-egorl@ya.ru>
2023-03-28 22:40:06 +0300

    WIP

--- bot/src/handlers/admins_handlers.py
+++ bot/src/handlers/admins_handlers.py
@@ -4,6 +4,6 @@ from aiogram import types
 async def admin_start_handler(message: types.Message):
     kb = types.ReplyKeyboardMarkup(keyboard=[
         # Todo: change url
-        [types.KeyboardButton(text="Сюда переходи", web_app=types.WebAppInfo(url="https://6a30-188-130-155-160.eu.ngrok.io/"))]
+        [types.KeyboardButton(text="Сюда переходи", web_app=types.WebAppInfo(url="https://5c5a-188-130-155-160.eu.ngrok.io"))]
     ])
     await message.answer("Привет, нажми на кнопку внизу (нужно бы переделать этот текст)", reply_markup=kb)
